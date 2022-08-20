f47bce0bf7f28292c1336caecdf9d1bfa9e1cbe8
Omar228228 <goblock542@gmail.com>
2022-08-20 13:54:29 +0300

    added 2 types of cherry, fixed bug with on/off colider

--- Assets/Scripts/Cherry.cs
+++ Assets/Scripts/Cherry.cs
@@ -1,13 +1,16 @@
+//using System;
 using System.Collections;
 using System.Collections.Generic;
+//using System.Diagnostics;
 using UnityEngine;
 
 public class Cherry : MonoBehaviour
 {
 
     private Vector2 size = new Vector2(1f, 1f);
-    private int richness = 1;
-
+    public int richness;
+    public int minRichness = -1;
+    public int maxRichness = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +25,23 @@ public class Cherry : MonoBehaviour
         
 
     }
-
-    //private void Respawn() {
-    //    Vector2 tempPos;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
         
-    //}
+    }
+
+    public void Respawn() {
+        //RectTransform parentCanvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+        //Vector2 canvasSize = new Vector2(parentCanvas.rect.width, parentCanvas.rect.height);
+        //Vector2 tempPos;
+        //tempPos.x = Random.Range(parentCanvas.position.x, ((canvasSize.x + parentCanvas.position.x) / size.x));
+        //tempPos.y = Random.Range(parentCanvas.position.y, ((canvasSize.y + parentCanvas.position.y) / size.y));
+        //transform.position = tempPos;
+        richness = Random.Range(minRichness, maxRichness);
+        Vector2 tempPos;
+        tempPos.x = Random.Range(-6, 6);
+        tempPos.y = Random.Range(-3, 3);
+        transform.position = tempPos;
+    }
+  
 }
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -6,14 +6,17 @@ public class Snake : MonoBehaviour
 {
     private int speed = 4;
     private Vector2 size = new Vector2(1f, 1f);
-    private GameObject[] Body;
+    private List<GameObject> Body = new List<GameObject>();
     private Vector2 destination = Vector2.right;
     private float timeCount;
-    
+    private Vector2 tempVel = new Vector2();
+    private Camera mainCamera;
+
+    public GameObject bodyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -21,21 +24,107 @@ public class Snake : MonoBehaviour
     {
         timeCount += Time.deltaTime;
         if (timeCount * speed >= 1) {
+            if (tempVel.magnitude > 0 && tempVel.magnitude <= 1 && tempVel.normalized != -destination.normalized)
+            {
+                destination = tempVel;
+            }
             timeCount = 0;
+            if (Body.Count > 0)
+            {
+                for (int i = 0; i < Body.Count - 1; i++)
+                {
+                    Body[Body.Count - i - 1].transform.position = Body[Body.Count - i - 2].transform.position;
+                    Body[Body.Count - i - 1].GetComponent<BoxCollider2D>().enabled = true;
+                }
+                //Debug.Log(Body.Count);//
+                Body[0].transform.position = transform.position - Vector3.back;
+                
+            }
             Vector2 tempPos = transform.position;
             tempPos.x += destination.x * size.x;
             tempPos.y += destination.y * size.y;
-            transform.position = tempPos;
+            //if (tempPos.x < mainCamera.rect.x || tempPos.x > mainCamera.rect.xMax) {
+            //    Debug.Log("Out of area x");
+            //    tempPos.x = ((tempPos.x - mainCamera.rect.x) / mainCamera.rect.width) - (int)((tempPos.x - mainCamera.rect.x) / mainCamera.rect.width); 
+            //}
+            //if (tempPos.y < mainCamera.rect.y || tempPos.y > mainCamera.rect.yMin)
+            //{
+            //    Debug.Log("Out of area y");
+            //    tempPos.y = ((tempPos.y - mainCamera.rect.y) / mainCamera.rect.height) - (int)((tempPos.y - mainCamera.rect.y) / mainCamera.rect.height);
+            //}
+            transform.position = new Vector3(tempPos.x, tempPos.y, -1);
+
         }
-        Vector2 tempVel;
-        tempVel.x = Input.GetAxisRaw("Horizontal");
-        tempVel.y = Input.GetAxisRaw("Vertical");
-        if (tempVel.magnitude > 0 && tempVel.magnitude <= 1) {
-            destination = tempVel;
+        Vector2 tempVel2; 
+
+        tempVel2.x = Input.GetAxisRaw("Horizontal");
+        tempVel2.y = Input.GetAxisRaw("Vertical");
+
+        if (tempVel2.magnitude > 0 && tempVel2.magnitude <= 1 && tempVel2.normalized != -tempVel.normalized)
+        {
+            tempVel = tempVel2;
+        }
+
+
+    }
+
+
+
+    private void GrowUp(int n) {
+        for(int i = 0; i < n; i++)
+        {
+
+            GameObject body = Instantiate(bodyPrefab, transform.position - Vector3.back, transform.rotation);
+            Body.Add(body);
+            body.transform.parent = transform.parent;
+
         }
         
+
     }
-    void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log("Collision!");
+    private void GrowDown(int n)
+    {       
+        for (int i = 0; i > n; i--)
+        {
+            GameObject body = Body[Body.Count - 1];
+            Body.RemoveAt(Body.Count - 1);
+            Destroy(body);
+        }
+
+
     }
+
+    private void GameOver() {
+
+        for (int i = 0; i < Body.Count; i++) {
+            Destroy(Body[i]);
+        }
+        Body = new List<GameObject>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Body")
+        {
+            GameOver();
+        }
+        else {
+            
+            Cherry cherry = collision.collider.GetComponent<Cherry>();
+            if (cherry.richness > 0) {
+                
+                GrowUp(cherry.richness);
+            }
+            else
+            {
+                GrowDown(cherry.richness);
+            }
+            //if( хв≥ст, отруйну €году)
+            cherry.Respawn();
+
+
+        }
+
+    }
+
 }
